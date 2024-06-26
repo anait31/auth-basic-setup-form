ba2a543efef975fc569345671a126bd50219eff7
anait31 <anaitullah198@gmail.com>
2024-06-26 12:32:16 +0800

    signIn With Popup and SignOut Integration

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,44 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
+import app from "../../firebase/firebase.init";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { useState } from "react";
 
 const Register = () => {
+
+    const [userInfo, setUserInfo] = useState('');
+    const [errorInfo, setErrorInfo] = useState('');
+    // const [loginLogoutState, setLoginLogoutState] = useState(false)
+
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+
+
+    const handleGoogleSignInPopup = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                console.log(result.user)
+                setUserInfo(result.user)
+            })
+            .catch(error => {
+                console.log(error.message);
+                setErrorInfo(error)
+            })
+    }
+
+    const handleLogoutButton = () => {
+        signOut(auth)
+        .then(result => {
+            setUserInfo('')
+            console.log(result)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="max-w-[1280px] mx-auto">
             <h2 className="text-center text-4xl capitalize font-bold">Please Register</h2>
@@ -37,6 +75,28 @@ const Register = () => {
                     <p>Already Have and Account? <Link to='/login' className="underline text-blue-500">Please login</Link></p>
                 </div>
             </form>
+            <div className="text-center">
+                <h1 className="text-xl font-bold">Or Sign In With</h1>
+                <div className="mt-3">
+                    {userInfo ? <button onClick={handleLogoutButton} className="btn btn-info text-white hover:bg-transparent hover:text-black duration-500">LogOut</button> :
+                    <div className="space-x-3">
+                        <button onClick={handleGoogleSignInPopup} className="btn btn-info text-white hover:bg-transparent hover:text-black duration-500">Google<FcGoogle /></button>
+                        <button className="btn btn-info text-white hover:bg-transparent hover:text-black duration-500">GitHub<FaGithub /></button>
+                        <button className="btn btn-info text-white hover:bg-transparent hover:text-black duration-500">Twitter<FaTwitterSquare /></button>
+                    </div>}
+                </div>
+                <div className="mt-3">
+                    {
+                        userInfo && <>
+                            <p className="text-xl">{userInfo.displayName}</p>
+                            <p>{userInfo.email}</p>
+                        </>
+                    }
+                    {
+                        errorInfo && <p>{errorInfo}</p>
+                    }
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtOtY8NejxmdFAwLpBm_jOL9V0fU_D8UI",
+  authDomain: "auth-form-50.firebaseapp.com",
+  projectId: "auth-form-50",
+  storageBucket: "auth-form-50.appspot.com",
+  messagingSenderId: "698823730261",
+  appId: "1:698823730261:web:04b8757f0baf1e98ebe0dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
